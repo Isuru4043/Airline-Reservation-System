547a3282db3cc778326c8636f9aaf7d6996eb6b1
Isuru4043 <isuru.akalanka4043@gmail.com>
2023-10-28 09:51:32 +0530

    create Management class

---
+++ AirLine reservation system/AirLine reservation system.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+class Management 
+{
+public:
+    Management()
+    {
+        
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
