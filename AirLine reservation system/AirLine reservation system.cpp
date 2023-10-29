ac37ad7126e96a5f8957b95991b9fcccb4c87cc9
Isuru4043 <isuru.akalanka4043@gmail.com>
2023-10-29 13:34:25 +0530

    create mainmenu  method

--- AirLine reservation system/AirLine reservation system.cpp
+++ AirLine reservation system/AirLine reservation system.cpp
@@ -2,14 +2,41 @@
 #include <fstream>
 #include <iomanip>
 
+using namespace std;
+
+void mainMenu();
+
 class Management 
 {
 public:
     Management()
     {
-        
+        mainMenu();
     }
 };
+
+void mainMenu() {
+    int lchoice;
+    int schoice;
+    int back;
+
+    cout << "\t            XYZ Airlines \n" << endl;
+    cout << "\t____________Main Menu_____________" << endl;
+
+    cout << "\t_____________________________________" << endl;
+    cout << "\t|\t\t\t\t\t|" << endl;
+
+    cout << "\t|\t Press 1 to add the customer Details   \t" << endl;
+    cout << "\t|\t Press 2 for flight registration   \t" << endl;
+    cout << "\t|\t Press 3 for ticket and charges  \t" << endl;
+    cout << "\t|\t Press 4 Exit   \t" << endl;
+    cout << "\t|\t\t\t\t\t|" << endl;
+    cout << "\t_____________________________________" << endl;
+
+    cout << " Enter the choice : ";
+    cin >> lchoice;
+
+}
 
 int main()
 {
